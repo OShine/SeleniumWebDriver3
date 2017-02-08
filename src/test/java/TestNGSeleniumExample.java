e9f9a347b005f3e2b6dfa9787d555de97ed66898
Denis <shklyannik.denis@gmail.com>
2017-02-08 15:54:50 +0300

    Second commit

--- src/test/java/TestNGSeleniumExample.java
+++ src/test/java/TestNGSeleniumExample.java
@@ -1,33 +1,19 @@
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 public class TestNGSeleniumExample {
-    private WebDriver driver = new FirefoxDriver();
-    private String baseUrl;
+    public String baseUrl = "https://192.168.100.26/";
+    public WebDriver driver = new FirefoxDriver();
 
-    @BeforeMethod
-    public void setUp() throws Exception {
-        driver =  new InternetExplorerDriver();
-        baseUrl = "http://EugenBorisik:qwerty12345@https://192.168.100.26/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void testUntitled() throws Exception {
+    @Test(priority = 1)
+    public  void verifyHomePageTitle() {
         driver.get(baseUrl);
-
-    }
-
-    @AfterMethod
-    public void tearDown() throws Exception {
+        String expectedTitle = "Expected Title";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
         driver.quit();
-
     }
+
 }
