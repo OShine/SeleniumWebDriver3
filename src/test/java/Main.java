0de7ba49833451cbc2172052363988cae9e54fcf
Denis <shklyannik.denis@gmail.com>
2017-02-13 13:37:44 +0300

    Login test

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,12 @@
+import org.junit.rules.Timeout;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -30,6 +35,17 @@ public class Main {
     public void main() {
 
         driver.get(baseUrl);
+        WebElement usernameElement = driver.findElement(By.cssSelector("#Username"));
+        WebElement passwordElement = driver.findElement(By.cssSelector("#Password"));
+        WebElement rememberElement = driver.findElement(By.cssSelector(".remember-chBox"));
+        WebElement loginElement = driver.findElement(By.cssSelector("#SubmitButton"));
+        WebElement signElement = driver.findElement(By.cssSelector(".sign-out-span"));
+
+        usernameElement.sendKeys("EugenBorisik");
+        passwordElement.sendKeys("qwerty12345");
+        rememberElement.click();
+        loginElement.click();
+        Assert.assertTrue(signElement.getText().contains("Sign Out"));
 
     }
 
