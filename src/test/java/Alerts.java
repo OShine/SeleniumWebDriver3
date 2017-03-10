a0c33859647109764c1e2d5be826d3dd1762f80e
Denis <shklyannik.denis@gmail.com>
2017-03-10 16:16:22 +0300

    7.	Create 3 tests for alerts (URL - https://the-internet.herokuapp.com/javascript_alerts).

--- src/test/java/Alerts.java
+++ src/test/java/Alerts.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
